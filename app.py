f58851103b540f0cfdf2d715cbaefdc269a4b305
jayantkulkarni <jayant.kulkarni@ibm.com>
2022-04-19 13:43:10 -0700

    Post email message change

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def send_email(instanceId, alarmName):
     #mail_content = " Hello,  This is a simple mail -- WfPs Test Email to verify Notifications are working -- sent using Python SMTP library. Thank You"
     mail_content = "You have a task assigned awaiting approval. Please approve  Closed Loop Automation:" + \
         instanceId + " for Alarm" + alarmName + \
-            "<a href="https://www.google.com/">click here</a>" 
+            "<a href=\"https://www.google.com/\" >click here</a>" 
 
     mail_content = mail_content + workplace_url
     # Setup the MIME
